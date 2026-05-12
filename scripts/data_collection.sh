45fe06d1235fe8aad6659376ff355ff3be179593
BogGoro <troyegubov.den@gmail.com>
2026-05-12 18:44:25 +0300

    add: presentation

--- scripts/data_collection.sh
+++ scripts/data_collection.sh
@@ -2,7 +2,7 @@ mkdir -p data
 
 echo "Downloading dataset files..."
 
-# It somehow cannot download from those links so I have to use kaggle link with only Oct and Nov
+# It somehow cannot download from those links so I have to use kaggle link with only Oct and Nov and looaded Dec to kaggle myself
 # wget -O data1/2019-Oct.csv.gz "https://data.rees46.com/datasets/marketplace/2019-Oct.csv.gz"
 # wget -O data1/2019-Nov.csv.gz "https://data.rees46.com/datasets/marketplace/2019-Nov.csv.gz"
 # wget -O data1/2019-Dec.csv.gz "https://data.rees46.com/datasets/marketplace/2019-Dec.csv.gz"
@@ -12,7 +12,9 @@ echo "Downloading dataset files..."
 # unzip data1/2019-Dec.csv.zip -d data1/
 
 wget -O data/data.zip "https://www.kaggle.com/api/v1/datasets/download/mkechinov/ecommerce-behavior-data-from-multi-category-store"
+wget -O data/data2.zip "https://www.kaggle.com/api/v1/datasets/download/boggoro/project-dataset"
 
 unzip data/data.zip -d data/
+unzip data/data2.zip -d data/
 
 echo "Data collection completed"
